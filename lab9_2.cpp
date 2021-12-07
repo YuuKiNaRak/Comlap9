b1aa272755c7108c37aede4ffce1de5e6bf2b2de
ํYuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2021-12-07 15:48:32 +0700

    Complete lap9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,36 +3,72 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+float findGrade(float x){
+    char y;
+    if(x > 90){
+        y='A';
+        
+    }
+    else{
+        if(x > 75){
+            y='B';
+            
+        }
+        else{
+            if(x > 60){
+                y='C';
+                
+            }
+            else{
+                if(x > 45){
+                    y='D';
+                    
+                }
+                else{
+                    if(x <= 45){
+                        y='F';
+                        
+                    }
+                    else{}
+                }
+            }
+        }
+    }
+    return y;
+}
+
+
 
 int main(){
-	//Input the number of students
-	int N,i = 0;
-	cout << "Enter the number of students: ";
-	cin >> N;
-	string name[N];
-	float score[N];	
-	
-	//Store names and scores of students into an array 
-	while(i < N){
-		cout << "Name of student " << i+1 << ": ";
-		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
-		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
-		i++;
-	}
-	
-	//Print names scores and grades
-	i = 0;
-	cout << "---------------------------------------------\n";
-	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
-	cout << "---------------------------------------------\n";
-	while(i < N){
-		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-		i++;
-	} 
-	cout << "---------------------------------------------\n";
 
-	return 0;
+
+    int N,i = 0;
+    cout << "Enter the number of students: ";
+    cin >> N;
+    string name[N];
+    float score[N];
+
+
+    while(i < N){
+        cout << "Name of student " << i+1 << ": ";
+        cin.ignore();
+        getline(cin,name[i]);
+        cout << "Score of student " << i+1 << ": ";
+        cin >> score[i];
+        i++;
+    }
+
+    i = 0;
+    char x;
+    cout << "---------------------------------------------\n";
+    cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
+    cout << "---------------------------------------------\n";
+    while(i < N){
+        x = findGrade(score[i]);
+        cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << x << "\n";
+        i++;
+    } 
+    cout << "---------------------------------------------\n";
+
+    return 0;
 }
